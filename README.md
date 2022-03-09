12440de088f914df52703bccb578980c64555e48
Ian Castleden <ian.castleden@gmail.com>
2022-03-09 14:04:10 +0800

    added sqlite

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # SQL.jl
+
 Easy Notebook julia SQL
 
-
-install with
+install with:
 
 ```julia
 Pkg.add(url="https://github.com/arabidopsis/SQL.jl")
@@ -11,9 +11,30 @@ Pkg.add(url="https://github.com/arabidopsis/SQL.jl")
 Use like:
 
 ```julia
-import SQL: @sql_df, mysql_connect
+import SQL: @sql_df, @sql_cmd, mysql_connect
 
 mysql_connect("mysql://username:$(password)@localhost/database")
 N = 5
 @sql_df "select * from table limit $(N)"
 ```
+
+Currently I can't find a way to do string interpolation
+for a custom command:
+
+```julia
+# this works
+df = sql`select * from table limit 5`
+
+N = 5
+# interpolation *does not* work
+df = sql`select * from table limit $(N)`
+
+# but this does!
+df = @sql_cmd "select * from table limit $(N)"
+```
+
+----
+
+## TODO
+
+* add postgresql too
--- src/SQL.jl
+++ src/SQL.jl
@@ -1,36 +1,69 @@
 module SQL
+
 import MySQL
 import DBInterface
 import DataFrames: DataFrame
 import URIs: URI
+import SQLite
 
 
-export mysql_connect, sql_df, set_db, @sql_cmd, @sql_df, @sql_cmd
+export sql_df, set_db, @sql_cmd, @sql_df, @sql_cmd, connect
 
 CC = nothing
 
-function mysql_connect(uri::String)
+function connect(uri::String)
     u = URI(uri)
+    if startswith(u.scheme, "mysql")
+        return mysql_connect(u)
+    end
+    if startswith(u.scheme, "sqlite")
+        return sqlite_connect(u)
+    end
+    throw(ArgumentError("not a known schema $(uri)"))
+end
+
+function mysql_connect(u::URI)
     user, password = split(u.userinfo, ":")
+    # need to be strings not substrings :(
+    user = String(user)
+    password = String(password)
+    host = String(u.host)
     if u.port == ""
         port = 3306
     else
         port = parse(Int, u.port)
     end
+    db = String(u.path[2:end])
+    query = filter(t -> t[1] == "unix_socket", [split(q, "=") for q in split(u.query, "&")])
+    if length(v) >= 1
+        unix_socket = String(query[1][2])
+    else
+        unix_socket = MySQL.API.MYSQL_DEFAULT_SOCKET
+    end
     @assert startswith(u.scheme, "mysql")
     global CC
-    CC = DBInterface.connect(MySQL.Connection, string(u.host), string(user), string(password); port = port, db = string(u.path[2:end]))
+    CC = DBInterface.connect(MySQL.Connection, host, user, password; port = port, db = db, unix_socket = unix_socket)
     CC
 end
 
-"Set default database connection to conn"
-function set_db(conn)
+function sqlite_connect(u::URI)
+    @assert startswith(u.scheme, "sqlite") "not an sqlite database"
+    db = String(u.path[2:end])
     global CC
+    CC = DBInterface.connect(SQLite.DB, db)
+    CC
+end
+
+"Set default database connection to conn or rest to nothing"
+function set_db(conn::Union{DBInterface.Connection,Nothing})::Union{DBInterface.Connection,Nothing}
+    global CC
+    old = CC
     CC = conn
+    old
 end
 "Execute an SQL query on database conn"
 function sql_df(conn, query::String)::DataFrame
-    @assert conn != nothing "no database connection!"
+    @assert conn !== nothing "no database connection!"
     DataFrame(DBInterface.execute(DBInterface.prepare(conn, query)))
 end
 
@@ -40,11 +73,11 @@ function sql_df(conn::String, query::String)::DataFrame
     sql_df(conn, query)
 end
 
-function sql_df(query::String)::DataFrame
+function sql_df(query::String)
     sql_df(CC, query)
 end
 
-macro sql_cmd(query)
+macro sql_cmd(query::String)
 
     return quote
         local q0 = @eval $query
